067ac62478f1715e621d5355d1f91149390bf07e
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-07-02 20:05:39 -0500

    added 6 albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,31 @@
 console.log('***** Music Collection *****')
+
+
+//Create a variable `collection` that starts as an empty array.
+let collection = [];
+
+/**
+ * @param title 
+ * @param artist
+ * @param yearPublished
+ * 
+ * @return new object containing the input parameters
+ */
+
+function addToCollection(title, artist, yearPublished) {
+    const album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(album);
+}
+addToCollection('Nevermind', 'Nirvana', '1991');
+addToCollection('Revolver', 'Beatles', '1966');
+addToCollection('Abbey Road', 'Beatles', '1969');
+addToCollection('Jagged Little Pill', 'Alanis Morissette', '1995');
+addToCollection('Evil Empire', 'Rage Against The Machine', '1996');
+addToCollection('Tragic Kingdom', 'No Doubt', '1995');
+console.table(collection);
+
+
